e24cd2dce42832a61516a94f6a7c1dacdc799e85
mina <73959934+trueharuu@users.noreply.github.com>
2022-09-21 11:24:52 -0500

    Update U2_L5_Activity_One.java

--- src/U2_L5_Activity_One.java
+++ src/U2_L5_Activity_One.java
@@ -1,17 +1,15 @@
 import shapes.*;
 
-public class U2_L5_Activity_One
-{
-  public static void main(String[] args)
-  {
+public class U2_L5_Activity_One {
+    public static void main(String[] args) {
 
-    final Circle a = new Circle(10.1);
-    final Circle b = new Circle(14.0);
-    final Circle c = new Circle(20.5);
-    
-    System.out.println(a);
-    System.out.println(b);
-    System.out.println(c);
+        final Circle a = new Circle(10.1);
+        final Circle b = new Circle(14.0);
+        final Circle c = new Circle(20.5);
 
-  }
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
 }
