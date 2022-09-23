23340906a2dc90e44f7cd8cc98c7879a70e911a1
mina <73959934+trueharuu@users.noreply.github.com>
2022-09-23 11:04:35 -0500

    Create U2_L7_Activity_One.java

---
+++ src/U2_L7_Activity_One.java
@@ -0,0 +1,14 @@
+public class U2_L7_Activity_One {
+  public static void main(String[] args)
+  {
+    int a = Integer.MAX_VALUE; // change 'long' to 'int';
+    a += 1; // add =
+
+    System.out.println(a);
+
+    int b = Integer.MIN_VALUE; // change 'long' to 'int';
+    b -= 1; // add =
+    
+    System.out.println(b); // remove ''
+  }
+}
