e238d6d5895f5ac5054e3a60ef5dcb9838eb5bfa
mina <73959934+trueharuu@users.noreply.github.com>
2022-09-23 11:31:53 -0500

    Create Math.java

---
+++ src/testing/Math.java
@@ -0,0 +1,31 @@
+package testing;
+
+public class Math {
+    public static int abs(int value) {
+        return java.lang.Math.abs(value);
+    }
+
+    public static long abs(long value) {
+        return java.lang.Math.abs(value);
+    }
+
+    public static float abs(float value) {
+        return java.lang.Math.abs(value);
+    }
+
+    public static double abs(double value) {
+        return java.lang.Math.abs(value);
+    }
+
+    public static double pow(double value, double exponent) {
+        return java.lang.Math.pow(value, exponent);
+    }
+
+    public static double random() {
+        return java.lang.Math.random();
+    }
+
+    public static double sqrt(double value) {
+        return java.lang.Math.sqrt(value);
+    }
+}
